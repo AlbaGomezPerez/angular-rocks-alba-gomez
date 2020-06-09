f53a323b871128ae0d174fd8e04f4f8e21f97ae8
Alba Gómez Pérez <albagope@gmail.com>
2020-06-09 15:01:20 +0200

    create first html structure

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-band',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  title = 'alba';
+}
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,7 +9,8 @@ import {BandsService, Band} from "./bands.service";
  */
 @Component({
   selector: 'app-list',
-  templateUrl: './list.component.html'
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
   //constructor(private http: HttpClient) {}
