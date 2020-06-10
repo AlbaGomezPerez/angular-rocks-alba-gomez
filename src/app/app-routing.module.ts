984b082a72f226f5af572082691567ca75cf6dd3
Alba Gómez Pérez <albagope@gmail.com>
2020-06-10 12:13:02 +0200

    add routes to list and card component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from "./app.component";
 import {ListComponent} from "./list/list.component";
 import {CardComponent} from "./card/card.component";
 
 
+
 const routes: Routes = [
-  {path: '', component: ListComponent},
+  {path: '',redirectTo:'/list', pathMatch: 'full'},
+  {path: 'list', component: ListComponent},
   {path: 'band', component: CardComponent}
 ];
 
