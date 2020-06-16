1cb2d2d210c379355e0fb388be0d2395c7ff7a06
Alba Gómez Pérez <albagope@gmail.com>
2020-06-16 12:40:34 +0200

    add tests to bandCard component

--- src/app/test/bandCard.component.spec.ts
+++ src/app/test/bandCard.component.spec.ts
@@ -1,19 +1,51 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { BandCardComponent } from '../bandCard/bandCard.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BandCardComponent} from '../bandCard/bandCard.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BandsService} from "../services/bands.service";
 
-describe('AppComponent', () => {
+describe('BandsComponent', () => {
+
   let fixture: ComponentFixture<BandCardComponent>;
+  let compiled;
+  let jsonService: BandsService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
       ],
       declarations: [
         BandCardComponent
       ],
+      providers: [
+        BandsService
+      ]
     }).compileComponents();
+    fixture = TestBed.createComponent(BandCardComponent);
+    compiled = fixture.nativeElement;
+
+    jsonService = TestBed.get(BandsService);
+
   }));
+
+  it('should show the detail card band', () => {
+    const card = compiled.querySelector('.bands');
+    expect(card).toBeTruthy();
+  });
+
+  it('should show the go back button', () => {
+    const backButton = compiled.querySelector('.back-button');
+    expect(backButton).toBeTruthy();
+    expect(backButton.getAttribute('routerLink')).toContain('/list');
+  });
+
+  it('should show the go back button', () => {
+    const contentCard = compiled.querySelector('.card-contain a');
+    expect(contentCard).toBeTruthy();
+    // expect(contentCard.getAttribute('href')).toContain("");/
+  });
 
 
 });
