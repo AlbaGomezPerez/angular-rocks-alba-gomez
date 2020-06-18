014c36b6891d443af62726adf3ee481fd296c71f
Alba Gómez Pérez <albagope@gmail.com>
2020-06-18 13:17:49 +0200

    remove unused files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import {BandsComponent} from "./bands/bands.component";
 import {BandCardComponent} from "./bandCard/bandCard.component";
 import {NewBandComponent} from "./newBand/newBand.component";
-import {PruebaComponent} from "./prueba/prueba.component";
 import {AppComponent} from "./app.component";
 
 
@@ -12,7 +11,6 @@ const routes: Routes = [
   {path: 'list', component: BandsComponent},
   {path: 'band/:id', component: BandCardComponent, pathMatch: 'prefix'},
   {path: 'form', component: NewBandComponent},
-  {path: 'prueba', component: PruebaComponent},
   {path: '**', component: AppComponent }
 ];
 
